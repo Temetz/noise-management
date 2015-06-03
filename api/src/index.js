8d38347437d88dcbd43129e454f22099cc0a3f2f
Teemu Kontio <teemu@t-palvelut.net>
2015-06-03 11:52:54 +0300

    Modified for teamboard use only

--- api/src/index.js
+++ api/src/index.js
@@ -5,15 +5,10 @@ var bodyParser      = require('body-parser');
 var cookieParser    = require('cookie-parser');
 
 // Constants
-var PORT = 8000;
+var PORT = 8002;
 
 // App
 var app = express();
-
-// Authentication
-app.locals.state = {
-    authenticated: false
-}
 
 app.use(bodyParser.json());
 app.use(cookieParser('verysecurepassword'));
@@ -22,36 +17,13 @@ app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.all('*', checkUser);
-
-function checkUser(req, res, next) {
-  var _ = require('underscore')
-      , nonSecurePaths = ['/', '/gui/login', '/login'];
-
-  if ( _.contains(nonSecurePaths, req.path) ){
-    if(app.locals.state.authenticated === true){
-        app.locals.state.authenticated = false;
-        return next();
-    }
-    return next();
-  }
-
-  if(req.cookies.apikey == process.env.APIKEY){
-        app.locals.state.authenticated = true;
-        next();
-    }
-    else {
-        app.locals.state.authenticated = false;
-        res.redirect('/gui/login');
-    }
-}
 
 app.get('/', function (req, res) {
     res.redirect('/gui/welcome');
 });
 
 app.get('/gui/welcome', function (req, res) {
-    res.render('welcome', {host:req.headers['host'], manage: req.cookies.sessionident});
+    res.render('welcome');
 });
 
 app.get('/gui/configure', function (req, res) {
@@ -60,37 +32,6 @@ app.get('/gui/configure', function (req, res) {
 
 app.get('/gui/target', function (req, res) {
     res.render('target');
-});
-
-app.get('/gui/login', function (req, res) {
-    res.render('login');
-});
-
-app.get('/gui/logout', function (req, res) {
-    res.render('logout');
-});
-
-app.post('/login', function (req, res) {
-    if(req.body.apikey == process.env.APIKEY)
-    {
-        res.cookie('apikey', req.body.apikey);
-        res.cookie('sessionident', req.body.sessionident);
-	res.cookie('sessionpassword', req.body.sessionpassword);
-        res.json("Login success");
-        return;
-    }
-    res.clearCookie('apikey', '');
-    res.clearCookie('sessionident', '');
-    res.clearCookie('sessionpassword', '');
-    res.status(401);
-    res.json("Invalid login");
-});
-
-app.post('/logout', function (req, res) {
-    res.clearCookie('apikey', '');
-    res.clearCookie('sessionident', '');
-    res.clearCookie('sessionpassword', '');
-    res.json("You have logged out");
 });
 
 app.listen(PORT);
